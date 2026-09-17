c3923013e88d791ed54023f84e678fdfd3bf3ee4
Andrei Makarov <andrei@kiskolabs.com>
2026-09-17 14:09:47 +0300

    Add JUnit formatter and coverage reporting to CI workflow
    - Introduced a new JUnit formatter for RSpec to support JSON to JUnit XML conversion in CI.
    - Updated the GitHub Actions workflow to report coverage to Sonar/Codecov and merge RSpec JSON outputs into JUnit format.
    - Enhanced the test setup to accommodate parallel test runs with coverage reporting capabilities.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,3 +38,4 @@ Polyrun::RSpec.install_example_prosopite!
 if %w[1 true yes].include?(ENV["POLYRUN_SPEC_QUALITY"]&.to_s&.downcase)
   Polyrun::RSpec.install_spec_quality!
 end
+require_relative "support/junit_formatter"
